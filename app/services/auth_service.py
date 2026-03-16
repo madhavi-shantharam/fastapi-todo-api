a1a06a21aa18b453acf88b7d02fa090e53909406
madhavi <madhavi.shantharam@gmail.com>
2026-03-15 21:18:18 -0700

    Refactor: Created .env files for secret keys

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -9,9 +9,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import User
 
-SECRET_KEY="supersecretkey"
-ALGORITHM="HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES=60
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES= int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
